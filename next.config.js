9219b48f7430e99f5f3bed54b0f7527200ea81c9
rizo8107 <nirmal@lifedemy.in>
2025-02-11 10:42:28 +0530

    Update Netlify configuration and Next.js settings

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Add jspdf as an external module
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        canvas: false,
-        'canvas-renderer': false
-      };
-    }
-    return config;
+  output: 'standalone',
+  images: {
+    unoptimized: true,
   },
   reactStrictMode: true,
-  swcMinify: true
 }
 
 module.exports = nextConfig
